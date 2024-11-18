4e91a5e1d0eae8dcd87633af4f1508e744d5cc8e
Kiki Mahesta <147258845+Kimhsta@users.noreply.github.com>
2024-11-18 19:48:10 +0700

    menambah gambar dan color ditailwin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#06b6d4',
+        dark: '#0f172a',
+      },
+    },
   },
   plugins: [],
 }
