8144b887abdcd5d282ad1cc0ff43f4a3e90717cd
Kiki Mahesta <147258845+Kimhsta@users.noreply.github.com>
2024-11-19 14:37:33 +0700

    pembuatan navbar

--- dist/JS/script.js
+++ dist/JS/script.js
@@ -1,3 +1,17 @@
+// Navbar
+window.onscroll = function() {
+    const header = document.querySelector('header');
+    const fixedNav = header.offsetTop;
+
+    if (window.pageYOffset > fixedNav) {
+        header.classList.add('navbar-fixed');
+    } else { 
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+
+// Hamburger Button
 const hamburger = document.querySelector('#hamburger');
 
 hamburger.addEventListener('click', function() {
