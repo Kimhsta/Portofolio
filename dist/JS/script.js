4c3abcc9f6b8a4619cf3588f0554282252ab167e
Kiki Mahesta <147258845+Kimhsta@users.noreply.github.com>
2024-11-19 14:20:36 +0700

    membuat hamburger csilang

--- dist/JS/script.js
+++ dist/JS/script.js
@@ -1,3 +1,5 @@
 const hamburger = document.querySelector('#hamburger');
 
-hamburger.addEventListener('click', function)
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active')
+});
